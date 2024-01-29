caf370d9b0f19afface1d794bde76314f8d0fdea
Radheranij <157111271+Radheranij@users.noreply.github.com>
2024-01-29 15:29:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "6189662ec5e102e7696e0aa6b2ff2adc"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = ""
+OWNER_ID = "2096463013"
 SUPPORT_GRP = "HEROKUFREECC"
 UPDATE_CHNL = "ALLTYPECC"
 OWNER_USERNAME = "iam_daxx"
